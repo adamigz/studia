188f42d2947f0c0afb3040b3b00a7f4d2a4ae610
Maciej Jandzinski <macio.j2004@wp.pl>
2023-10-29 18:46:04 +0100

    add file

---
+++ laboratoria/29.10/zadanie9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int x1, x2, y1, y2, a, b;
+	printf("Podaj wspolrzedne wierzcholka A: ");
+	scanf("%d %d", &x1, &y1);
+	printf("Podaj wspolrzedne wierzcholka B: ");
+        scanf("%d %d", &x2, &y2);
+	// ZA DUZO IFÓW :shrug:
+}
