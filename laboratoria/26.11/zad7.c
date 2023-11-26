72391ab7a08163be1892fc79532d5737d814a701
adamigz <47462938+adamigz@users.noreply.github.com>
2023-11-26 11:01:13 +0100

    edit file

--- laboratoria/26.11/zad7.c
+++ laboratoria/26.11/zad7.c
@@ -2,12 +2,21 @@
 
 int tab[10] = {1,2,3,4,5,6,7,8,9,10};
 
-int contains(int n) {
-
+int contains(int n, int i) {
+    int size = sizeof(tab)/sizeof(tab[0]);
+    if (i<size) {
+        if (n==tab[i]) {
+            return 1;
+        } else {
+            return contains(n, ++i);
+        }
+    } else {
+        return 0;
+    }
 }
 
 int main() {
-    printf("7: %d\n", contains(7));
-    printf("20: %d", contains(20));
+    printf("7: %d\n", contains(7, 0));
+    printf("20: %d", contains(20, 0));
     return 0;
 }
